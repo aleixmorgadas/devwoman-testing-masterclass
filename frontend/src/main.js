3041aa88b629be05f1baa51152ed48b3f4cd1399
Aleix Morgadas <hello@aleixmorgadas.dev>
2025-02-12 16:38:11 +0100

    create match

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,32 @@
-fetch(`http://localhost:3000/matches`, {
-  method: 'GET'
-}).then(async response => {
-  const matches = await response.json();
-  const view = matches
-    .map(match => renderMatch(match))
-    .reduce((acc, current) => acc + current, '');
-
-  const temp = document.createElement('div');
-  temp.innerHTML = view;
-  const matchesDiv = document.querySelector('#matches');
-  while (temp.firstChild) {
-    matchesDiv.appendChild(temp.firstChild);
-  }
-})
+function fetchMatches() {
+  fetch(`http://localhost:3000/matches`, {
+    method: 'GET'
+  }).then(async response => {
+    const matches = await response.json();
+    const view = matches
+      .map(match => renderMatch(match))
+      .reduce((acc, current) => acc + current, '');
+  
+    const temp = document.createElement('div');
+    temp.innerHTML = view;
+    const matchesDiv = document.querySelector('#matches');
+    while (temp.firstChild) {
+      matchesDiv.appendChild(temp.firstChild);
+    }
+  });  
+}
 
 document.querySelector('#app').innerHTML = `
   <div>
+    <div class="create-match">
+      <label for="player1">Player1</label>
+      <input type="text" id="player1"/>
+
+      <label for="player2">Player2</label>
+      <input type="text" id="player2"/>
+
+      <button id="create-match-button">Create new match</button>
+    </div>
     <div id="matches" class="matches">
       <div class="row header">
         <div>ID</div>
@@ -27,6 +38,23 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
+document.getElementById('create-match-button').addEventListener('click', async () => {
+  const player1Name = document.getElementById('player1').value;
+  const player2Name = document.getElementById('player2').value;
+  
+  await fetch(`http://localhost:3000/matches`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      player1: player1Name,
+      player2: player2Name
+    }),
+  });
+  location.reload();
+});
+
 const renderMatch = (match) => {
   return `<div class="row">
       <div>${match._id}</div>
@@ -36,3 +64,5 @@ const renderMatch = (match) => {
     </div>  
     `
 };
+
+fetchMatches();
